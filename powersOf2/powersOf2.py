a5b895eed30f71fa5f91eb4174b66f5f9ea2c62a
terereus2 <62788115+terereus2@users.noreply.github.com>
2022-10-11 21:31:48 +0300

    Update powersOf2.py

--- powersOf2/powersOf2.py
+++ powersOf2/powersOf2.py
@@ -1,18 +1,2 @@
-'''
-Complete the function that takes a non-negative integer n as input, 
-and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
-
-Examples
-n = 0  ==> [1]        # [2^0]
-n = 1  ==> [1, 2]     # [2^0, 2^1]
-n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
-
-def basic_test_cases():
-        test.assert_equals(powers_of_two(0), [1])
-        test.assert_equals(powers_of_two(1), [1, 2])
-        test.assert_equals(powers_of_two(4), [1, 2, 4, 8, 16])
-'''
-
-
 def powers_of_two(n):
     return [2**elem for elem in range(n+1)]
